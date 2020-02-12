bf3edb98f5a7feff4d2e6e935fadfe5fa5518c41
Emma <emmagray183@gmail.com>
2020-02-12 20:21:13 +0000

    routes for whiskies by region

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -30,10 +30,16 @@ public class WhiskyController {
 //        return new ResponseEntity<>(whiskyRepository.findByYear(year), HttpStatus.OK);
 //    }
 
+//    @GetMapping(value = "/whiskies")
+////    public ResponseEntity<List<Whisky>> findWhiskiesFromCertainDistilleryByAge(
+////            @RequestParam(name="distillery") String distillery, @RequestParam(name="age") int age){
+////        return new ResponseEntity<>(whiskyRepository.findByDistilleryNameAndAge(distillery, age), HttpStatus.OK);
+////    }
+
     @GetMapping(value = "/whiskies")
-    public ResponseEntity<List<Whisky>> findWhiskiesFromCertainDistilleryByAge(
-            @RequestParam(name="distillery") String distillery, @RequestParam(name="age") int age){
-        return new ResponseEntity<>(whiskyRepository.findByDistilleryNameAndAge(distillery, age), HttpStatus.OK);
+    public ResponseEntity<List<Whisky>> findAllWhiskiesFromRegion(
+            @RequestParam(name="region") String region){
+        return new ResponseEntity<>(whiskyRepository.findByDistilleryRegion(region), HttpStatus.OK);
     }
 
 }
